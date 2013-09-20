cdd2c25c92616978ad4634c701982b3c17dd55a6
Mark Silverberg <mark@socialhealthinsights.com>
2013-09-20 16:05:53 -0400

    minor whitespace changes

--- spec/lib/truevault/client_spec.rb
+++ spec/lib/truevault/client_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../../spec_helper'
 
-describe TrueVault::Client do
-  
+describe TrueVault::Client do  
   it "must include httparty methods" do
   	TrueVault::Client.must_include HTTParty
   end
@@ -9,6 +8,4 @@ describe TrueVault::Client do
   it "must have the base url set to the TrueVault endpoint" do
   	TrueVault::Client.base_uri.must_equal 'https://api.truevault.com'
 	end
-  
-
 end
