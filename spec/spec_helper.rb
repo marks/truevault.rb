a7ee0c1ac0b867e27140d28f9c056c6034869717
Mark Silverberg <mark@socialhealthinsights.com>
2013-09-21 11:02:45 -0400

    convert indentations to spaces

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,18 +8,18 @@ require 'vcr'
 require 'turn'
 
 Turn.config do |c|
-  c.format  = :pretty
-  c.natural = true
+	c.format  = :pretty
+	c.natural = true
 end
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/fixtures/truevault_cassettes'
-  c.hook_into :webmock
-  c.default_cassette_options = { :match_requests_on => [:method] }
-  c.before_record do |interaction|
-  	interaction.request.body.gsub!(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/, 'REDACTED_ID')
-  	interaction.request.uri.gsub!(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/, 'REDACTED_ID')
-    interaction.response.body.gsub!(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/, 'REDACTED_ID')
-  end
+	c.cassette_library_dir = 'spec/fixtures/truevault_cassettes'
+	c.hook_into :webmock
+	c.default_cassette_options = { :match_requests_on => [:method] }
+	c.before_record do |interaction|
+		interaction.request.body.gsub!(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/, 'REDACTED_ID')
+		interaction.request.uri.gsub!(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/, 'REDACTED_ID')
+		interaction.response.body.gsub!(/(\S{8}-\S{4}-\S{4}-\S{4}-\S{12})/, 'REDACTED_ID')
+	end
 
 end
