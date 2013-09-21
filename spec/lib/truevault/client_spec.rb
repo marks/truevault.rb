b9ede953574905a94d3154fbec8ccbcd9935839a
Mark Silverberg <mark@socialhealthinsights.com>
2013-09-21 10:40:33 -0400

    changing name of cassettes.. still getting the hang of this

--- spec/lib/truevault/client_spec.rb
+++ spec/lib/truevault/client_spec.rb
@@ -8,19 +8,32 @@ describe TrueVault::Client do
   it "must have the base url set to the TrueVault endpoint" do
   	TrueVault::Client.base_uri.must_equal 'https://api.truevault.com'
 	end
+
+
+	describe "GET list of vaults" do
+		let(:client){ TrueVault::Client}
+
+		before do
+			VCR.insert_cassette 'list_vaults', :record => :new_episodes
+		end
+
+		after do
+			VCR.eject_cassette
+		end
+
+		# it "records the fixture" do
+		# 	tv = TrueVault::Client.new(ENV["TV_API_KEY"], ENV["TV_ACCOUNT_ID"], 'v1')
+		# 	tv.list_vaults
+		# end
+
+		it "must respond to list_vaults" do 
+			puts "hi"
+			client.class.must_equal TrueVault::Client
+		end
+	end
+
+
+
+
 end
 
-describe "GET list of vaults" do
-	before do
-		VCR.insert_cassette 'vaults', :record => :new_episodes
-	end
-
-	after do
-		VCR.eject_cassette
-	end
-
-	it "records the fixture" do
-		tv = TrueVault::Client.new(ENV["TV_API_KEY"], ENV["TV_ACCOUNT_ID"], 'v1')
-		tv.list_vaults
-	end
-end
