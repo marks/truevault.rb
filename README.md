31d0588b3694771bb7b9d0f18f4b75ebb33c1b60
Mark Silverberg <mark@socialhealthinsights.com>
2013-09-21 20:16:22 -0400

    adding more tests, restructing some existing ones

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A super quick Ruby client for TrueVault.com based off of HTTParty.
 How to use
 ----------
 1. Clone this repository. Notice that all the code is in `truevault.rb`
-2. Set up the following environment variables to be used by `example_usage.rb`: `TV_API_KEY`, `TV_A_VAULT_ID`, `TV_ACCOUNT_ID`. 
+2. Set up the following environment variables to be used by `example_usage.rb`: `TV_API_KEY` and `TV_ACCOUNT_ID`, `TV_A_VAULT_ID`. (These environment variables are also used by tests if you want to recreate your own test cassettes.)
 3. Run `ruby example_usage.rb`
 4. Profit and then contributing back to an open source project
 
--- spec/lib/truevault/client_spec.rb
+++ spec/lib/truevault/client_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../../spec_helper'
 
 describe TrueVault::Client do  
-  
-  it "must include httparty methods" do
-  	TrueVault::Client.must_include HTTParty
+  random_string = (0...10).map { (65 + rand(26)).chr }.join
+  let(:client){ TrueVault::Client.new(random_string)}
+
+  it "parent class must include httparty methods" do
+  	client.class.must_include HTTParty
   end
 
-  it "must have the base url set to the TrueVault endpoint" do
-  	TrueVault::Client.base_uri.must_equal 'https://api.truevault.com'
+  it "parent class must have the base url set to the TrueVault endpoint" do
+  	client.class.base_uri.must_equal 'https://api.truevault.com'
 	end
 
   it "must require one argument (API key) upon instantiation" do
@@ -15,47 +17,83 @@ describe TrueVault::Client do
   end
 
   it "must store the first argument as the API key" do
-    random_string = (0...10).map { (65 + rand(26)).chr }.join
-    TrueVault::Client.new(random_string).instance_variable_get("@api_key").must_equal random_string
+    client.instance_variable_get("@api_key").must_equal random_string
   end
 
-	describe "GET list of vaults" do
-		let(:client){ TrueVault::Client.new('anything')}
+  methods = [:list_vaults, :create_document, :get_document, :update_document, :delete_document]
+  methods.each do |method|
+    it "must respond to method '#{method}'" do
+      client.must_respond_to method
+    end
+  end
+  
+  describe "list vaults" do
+    let(:list_vaults){ TrueVault::Client.new('anything').list_vaults}
 
-		before do
-			VCR.insert_cassette 'list_vaults'
-		end
+    before do
+      VCR.insert_cassette 'list_vaults'
+    end
 
-		after do
-			VCR.eject_cassette
-		end
+    after do
+      VCR.eject_cassette
+    end
 
-		it "records the fixture" do
-			tv = TrueVault::Client.new(ENV["TV_API_KEY"], ENV["TV_ACCOUNT_ID"], 'v1')
-			tv.list_vaults
-		end
-
-		it "must respond to list_vaults" do 
-			client.must_respond_to :list_vaults
-		end
+    it "records the fixture" do
+      tv = TrueVault::Client.new(ENV["TV_API_KEY"], ENV["TV_ACCOUNT_ID"], 'v1')
+      tv.list_vaults
+    end
 
     it "must parse the API response from JSON to a Ruby Hash" do
-      client.list_vaults.must_be_instance_of Hash
+      list_vaults.must_be_instance_of Hash
     end
 
     it "must have an array of vaults" do
-      client.list_vaults['vaults'].must_be_instance_of Array
+      list_vaults['vaults'].must_be_instance_of Array
     end
 
     it "must have been a successful API request" do
-      client.list_vaults['result'].must_equal "success"
+      list_vaults['result'].must_equal "success"
     end
 
     it "must have a redacted transaction_id " do
-      client.list_vaults['transaction_id'].must_equal REDACTED_STRING
+      list_vaults['transaction_id'].must_equal REDACTED_STRING
+    end
+  end
+
+
+  describe "create a document" do
+    let(:create_document){ TrueVault::Client.new('anything').create_document('a_vault_id',{"a" => "document"})}
+
+    before do
+      VCR.insert_cassette 'create_document'
     end
 
-	end
+    after do
+      VCR.eject_cassette
+    end
+
+    it "records the fixture" do
+      tv = TrueVault::Client.new(ENV["TV_API_KEY"], ENV["TV_ACCOUNT_ID"], 'v1')
+      tv.create_document(ENV["TV_A_VAULT_ID"], {"a key" => "a value", "an array key" => [0,1,2,3], "a hash key" => {"a" => "b", "x" => "y"}})
+    end
+
+    it "must parse the API response from JSON to a Ruby Hash" do
+      create_document.must_be_instance_of Hash
+    end
+
+    it "must have been a successful API request" do
+      create_document['result'].must_equal "success"
+    end
+
+    it "must have a redacted transaction_id" do
+      create_document['transaction_id'].must_equal REDACTED_STRING
+    end
+
+    it "must have a redacted document_id" do
+      create_document['document_id'].must_equal REDACTED_STRING
+    end
+
+  end
 
 end
 
