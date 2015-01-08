33294982203ecf18ca5e2b128f8677dd7b2cfbe2
Charles Massry <charlesdmassry@gmail.com>
2015-01-08 16:23:11 -0500

    crud vaults

--- lib/truevault/vault.rb
+++ lib/truevault/vault.rb
@@ -2,9 +2,45 @@ require "truevault/client"
 
 module TrueVault
   class Vault < Client
-    def all(options = {})
-      options.merge!(default_options_to_merge_with)
-      self.class.get("/#{@api_ver}/accounts/#{@account_id}/vaults", options)
+    #####################################
+    ###       Vault API Methods       ###
+    #####################################
+
+    # list all vaults
+    # TVVault.all
+
+    def all
+      self.class.get("/#{@api_ver}/vaults", default_options_to_merge_with)
+    end
+
+    # creates a vault
+    # TVVault.create_vault("name")
+
+    def create(name)
+      options = default_options_to_merge_with.merge({ query: { name: name } })
+      self.class.post("/#{@api_ver}/vaults", options)
+    end
+
+    # find a vault
+    # TVVault.find("00000000-0000-0000-0000-000000000000")
+
+    def find(vault_id)
+      self.class.get("/#{@api_ver}/vaults/#{vault_id}", default_options_to_merge_with)
+    end
+
+    # update a vault
+    # TVVault.update("00000000-0000-0000-0000-000000000000", "name")
+
+    def update(vault_id, name)
+      options = default_options_to_merge_with.merge({ query: { name: name } })
+      self.class.put("/#{@api_ver}/vaults/#{vault_id}", options)
+    end
+
+    # delete a vault
+    # TVVault.update("00000000-0000-0000-0000-000000000000")
+
+    def delete(vault_id)
+      self.class.delete("/#{@api_ver}/vaults/#{vault_id}", default_options_to_merge_with)
     end
   end
 end
